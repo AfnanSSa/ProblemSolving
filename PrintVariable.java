313da12b824ff193d0f9c2a3d207e13e836ff828
AfnanSSa <iafnanz67@gmail.com>
2024-02-07 09:05:58 +0400

    Printing Variable

---
+++ PrintVariable.java
@@ -0,0 +1,8 @@
+//Declare a variable userAge of type int and assign it your age. Print the variable.
+
+class ProblemSolving {
+    public static void main(String[] args) {
+        int userAge =23;
+        System.out.println("Age: " + userAge);
+    }
+}
