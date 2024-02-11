b09593a697c97ad4521236076d4fa9cc6ad609c9
AfnanSSa <iafnanz67@gmail.com>
2024-02-11 10:41:00 +0400

    Calculating tringle area

---
+++ TringleArea.java
@@ -0,0 +1,16 @@
+//Write a program that calculates and prints the area of a triangle with given base and height.
+
+public class ProblemSolving {
+    public static void main(String[] args) {
+        // Define base and hi=eight 
+        double base = 3.4;
+        double height = 4;
+        
+        //Calculate area of triangle
+        double area = 0.5 * base * height;
+
+        //Print result
+        System.out.println("Area of the tringle is: " + area);
+       
+    }
+}
