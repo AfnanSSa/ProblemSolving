aa4ffc4b671a9f1974f9f7e3a429b99aabfda0cf
AfnanSSa <iafnanz67@gmail.com>
2024-02-11 10:18:43 +0400

    converting inches to cm

---
+++ InchToCM.java
@@ -0,0 +1,15 @@
+//Write a program that converts inches to centimeters and prints the result.
+
+public class ProblemSolving {
+    public static void main(String[] args) {
+        // Define inch value
+        double inches = 2; 
+        
+        // Convert to centimeters
+        double centimeters = inches * 2.54;
+
+        //Print result
+        System.out.println(inches + " inches are equal to " + centimeters + " cm");
+       
+    }
+}
