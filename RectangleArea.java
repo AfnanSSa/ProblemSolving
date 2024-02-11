eee25518605172dbc3f8cf69a7f89370f3f06009
AfnanSSa <iafnanz67@gmail.com>
2024-02-11 10:23:52 +0400

    Calculating rectanle area

---
+++ RectangleArea.java
@@ -0,0 +1,15 @@
+//Write a program that calculates the area of a rectangle given its length and width and prints the result.
+
+public class ProblemSolving {
+    public static void main(String[] args) {
+        // Define length & width
+        double length = 9.5;
+        double width = 18;
+        //area of rectangle
+        double rectangleArea = length * width;
+        
+        //printig results
+        System.out.println("Are of the rectangle: " + rectangleArea);
+       
+    }
+}
